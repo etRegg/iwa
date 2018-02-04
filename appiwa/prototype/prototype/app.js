432c76c7ffdb4936b61c8246d24df14a680ea0ba
e <rodrigo@localhost>
2018-02-04 17:55:31 -0300

    add security to express

--- appiwa/prototype/prototype/app.js
+++ appiwa/prototype/prototype/app.js
@@ -1,11 +1,18 @@
+
+
+
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var auth = require('./routes/login');
 var contacts = require('./routes/contacts');
 var campagnings = require('./routes/campagnings');
 var app = express();
+app.use('/rui/redirect', express.static('node_modules/angular-ui-router-redirect/dist'));
+app.use('/rui', express.static('node_modules/angular-ui-router/release'));
 app.use('/js', express.static('bower_components'));
 app.use('/', express.static('public'));
 app.use(function(req, res, next) {
@@ -17,6 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+app.use('/api/v1/auth', auth);
 app.use('/api/v1/contacts', contacts);
 app.use('/api/v1/campagnings', campagnings);
 module.exports = app;
